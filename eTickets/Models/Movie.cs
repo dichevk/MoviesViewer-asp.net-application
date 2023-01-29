43795f26f5accee37b51e178320ad474147d16c8
Konstantin Dichev <100804008+dichevk@users.noreply.github.com>
2023-01-30 00:55:14 +0100

    models added

---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using eTickets.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+
+        public double Price { get; set; }
+        /*
+        public Cinema Cinema { get; set; }
+
+        public List <Actor> Actors { get; set; }
+        */
+        public MovieCategory MovieCategory { get; set; }
+        public string? Producer { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
