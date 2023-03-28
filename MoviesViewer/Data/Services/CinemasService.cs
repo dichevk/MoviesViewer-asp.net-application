3bbf3f7eeaf5f3feecda999becf808358d1d71a6
KonstantinDichev <kdichev.nl@gmail.com>
2023-03-28 02:08:18 +0200

    implemented Cinema Service

--- MoviesViewer/Data/Services/CinemasService.cs
+++ MoviesViewer/Data/Services/CinemasService.cs
@@ -1,6 +1,32 @@
-﻿namespace MoviesViewer.Data.Services
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesViewer.Models;
+
+namespace MoviesViewer.Data.Services
 {
     public class CinemasService : ICinemasService
     {
+        private readonly AppDbContext _context;
+        public CinemasService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void AddCinema(Cinema cinema)
+        {
+            throw new NotImplementedException();
+        }
+        public void DeleteCinema(Cinema cinema)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<Cinema> GetCinema(Cinema cinema)
+        {
+            var result = await _context.Cinemas.FindAsync(cinema);
+            return result;
+        }
+        public async Task<IEnumerable<Cinema>> GetCinemas()
+        {
+            var result = await _context.Cinemas.ToListAsync();
+            return result;
+        }
     }
 }
