d73b18f18d477d5b85a89534233d40b40a50ff71
KonstantinDichev <kdichev.nl@gmail.com>
2023-03-31 21:33:28 +0200

    Producers Service and interface

--- MoviesViewer/Data/Services/IProducersService.cs
+++ MoviesViewer/Data/Services/IProducersService.cs
@@ -1,6 +1,14 @@
-﻿namespace MoviesViewer.Data.Services
+﻿using MoviesViewer.Models;
+
+namespace MoviesViewer.Data.Services
 {
-    public class IProducersService
+    public interface IProducersService
     {
+        Task<IEnumerable<Producer>> GetProducers();
+        Task<Producer> GetProducer(Producer producer);
+        Task<Producer> GetProducerById(string id);
+        void AddProducer(Producer producer);
+        Producer UpdateProducer(string id, Producer producer);
+        void DeleteProducer(Producer producer);
     }
 }
--- MoviesViewer/Data/Services/ProducersService.cs
+++ MoviesViewer/Data/Services/ProducersService.cs
@@ -1,6 +1,45 @@
-﻿namespace MoviesViewer.Data.Services
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesViewer.Models;
+
+namespace MoviesViewer.Data.Services
 {
     public class ProducersService : IProducersService
     {
+        private readonly AppDbContext _context;
+        public ProducersService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void AddProducer(Producer producer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProducer(Producer producer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Producer> GetProducer(Producer producer)
+        {
+            var result = await _context.Producers.FindAsync(producer);
+            return result; 
+        }
+
+        public Task<Producer> GetProducerById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Producer>> GetProducers()
+        {
+            var result = await _context.Producers.ToListAsync();
+            return result; 
+        }
+
+        public Producer UpdateProducer(string id, Producer producer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
