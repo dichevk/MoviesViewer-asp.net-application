e82f4c01c77bd23945d8b40045f27c8eb49adbb0
KonstantinDichev <kdichev.nl@gmail.com>
2023-04-04 23:39:16 +0200

    Orders migration

---
+++ MoviesViewer/Migrations/20230404213902_OrdersAdded.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoviesViewer.Migrations
+{
+    public partial class OrdersAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Cinemas_CinemaId",
+                table: "Movies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Movies_CinemaId",
+                table: "Movies");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CinemaId1",
+                table: "Movies",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CinemaId",
+                table: "Cinemas",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_CinemaId1",
+                table: "Movies",
+                column: "CinemaId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Cinemas_CinemaId1",
+                table: "Movies",
+                column: "CinemaId1",
+                principalTable: "Cinemas",
+                principalColumn: "CinemaId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Cinemas_CinemaId1",
+                table: "Movies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Movies_CinemaId1",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "CinemaId1",
+                table: "Movies");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CinemaId",
+                table: "Cinemas",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_CinemaId",
+                table: "Movies",
+                column: "CinemaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Cinemas_CinemaId",
+                table: "Movies",
+                column: "CinemaId",
+                principalTable: "Cinemas",
+                principalColumn: "CinemaId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
