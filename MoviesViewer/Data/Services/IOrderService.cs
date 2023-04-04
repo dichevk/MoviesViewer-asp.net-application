0111ab658581fb4645604f263403ae876eba83d7
KonstantinDichev <kdichev.nl@gmail.com>
2023-04-04 23:35:54 +0200

    added orders service and interface

---
+++ MoviesViewer/Data/Services/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using MoviesViewer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace MoviesViewer.Data.Services
+{
+    public interface IOrdersService
+    {
+        Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress);
+        Task<List<Order>> GetOrdersByUserIdAndRoleAsync(string userId, string userRole);
+    }
+}
---
+++ MoviesViewer/Data/Services/OrdersService.cs
@@ -0,0 +1,17 @@
+﻿using MoviesViewer.Models;
+
+namespace MoviesViewer.Data.Services
+{
+    public class OrdersService : IOrdersService
+    {
+        public Task<List<Order>> GetOrdersByUserIdAndRoleAsync(string userId, string userRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
