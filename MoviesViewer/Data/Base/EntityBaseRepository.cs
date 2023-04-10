02273f594e9162c2171c558dc8444dd7aef1a7d1
KonstantinDichev <kdichev.nl@gmail.com>
2023-04-10 22:14:48 +0200

    EntityBase and IEntityBase Repo added

---
+++ MoviesViewer/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq.Expressions;
+
+namespace MoviesViewer.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, new()
+    {
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id, params Expression<Func<T, object>>[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(int id, T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MoviesViewer/Data/ViewComponents/ShoppingCartViewSummary.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesViewer.Data.ViewComponents
+{
+    public class ShoppingCartViewSummary:ViewComponent
+    {
+        public ShoppingCartViewSummary()
+        {
+        
+        }
+    }
+}
