aa75dad0cfe1399d9141704523e3b31d9e9cc7f7
KonstantinDichev <kdichev.nl@gmail.com>
2023-04-11 23:57:54 +0200

    Shoppingcart update

--- MoviesViewer/Models/ShoppingCartItem.cs
+++ MoviesViewer/Models/ShoppingCartItem.cs
@@ -9,6 +9,7 @@ namespace MoviesViewer.Models
 
         public Movie Movie { get; set; }
         public int Amount { get; set; }
+        public int Price { get; set; }
 
         public string ShoppingCartId { get; set; }
     }
