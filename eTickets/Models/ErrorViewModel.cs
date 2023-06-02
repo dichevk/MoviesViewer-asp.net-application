f80b228a5b1e7c8999b4632a8d8e79580bf04fdb
Konstantin Dichev <kdichev.nl@gmail.com>
2023-06-03 00:09:07 +0200

    Delete dup directory

--- eTickets/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MoviesViewer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
